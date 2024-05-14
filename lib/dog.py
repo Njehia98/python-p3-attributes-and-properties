432a464b19b9a4c112e5de1595e0a37f54334156
Njehia98 <stacynjihia028@gmail.com>
2024-05-14 13:17:53 +0300

    added functionality

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,24 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+   def __init__(self, name="Unknown", breed="Unknown"):
+        self.name = name
+        self.breed = breed
+
+   def name(self):
+        return self._name
+
+   def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value
+
+   def breed(self):
+        return self._breed
+
+   def breed(self, value):
+        if value not in self.approved_breeds:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = value
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3#!/usr/bin/env python3
 
 APPROVED_JOBS = [
     "Admin",
@@ -16,4 +16,24 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+   def __init__(self, name="Unknown", job="Unknown"):
+        self.name = name
+        self.job = job
+
+   def name(self):
+        return self._name
+
+   def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be a string between 1 and 25 characters.")
+        else:
+            self._name = value.title()
+
+   def job(self):
+        return self._job
+
+   def job(self, value):
+        if value not in self.approved_jobs:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = value
